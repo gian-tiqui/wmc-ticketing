06655bd0908c257c3b3b43e6c2eed646b81cfa41
gian-tiqui <gian.tiqui.dev@gmail.com>
2025-04-03 15:43:33 +0800

    added auto logout when refresh fails

--- src/@utils/http-common/apiClient.ts
+++ src/@utils/http-common/apiClient.ts
@@ -14,13 +14,15 @@ const isTokenExpired = (token: string) => {
   if (!token) return true;
 
   const { exp } = jwtDecode(token);
-
   if (!exp) throw new Error("Token does not have a valid exp");
-  if (Date.now() >= exp * 1000) {
-    return true;
-  }
 
-  return false;
+  return Date.now() >= exp * 1000;
+};
+
+const removeTokensAndReload = () => {
+  localStorage.removeItem(Namespace.BASE);
+  Cookies.remove(Namespace.BASE);
+  window.location.reload();
 };
 
 apiClient.interceptors.request.use(
@@ -30,31 +32,27 @@ apiClient.interceptors.request.use(
     if (accessToken && isTokenExpired(accessToken)) {
       try {
         const refreshToken = Cookies.get(Namespace.BASE);
+        if (!refreshToken) {
+          removeTokensAndReload();
+          throw new Error("No refresh token found");
+        }
 
         const response = await axios.post(
           `${apiUri}/api/v1/auth/refresh`,
           { refreshToken },
-          {
-            withCredentials: true,
-          }
+          { withCredentials: true }
         );
 
         accessToken = response.data.accessToken;
-
-        if (!accessToken || accessToken === undefined) {
-          Cookies.remove(Namespace.BASE);
-          localStorage.removeItem(Namespace.BASE);
-
+        if (!accessToken) {
+          removeTokensAndReload();
           throw new Error("No token was generated");
         }
 
-        try {
-          localStorage.setItem(Namespace.BASE, accessToken);
-        } catch (error) {
-          console.error(error);
-        }
+        localStorage.setItem(Namespace.BASE, accessToken);
       } catch (refreshError) {
         console.error("Token refresh failed:", refreshError);
+        removeTokensAndReload();
         return Promise.reject(refreshError);
       }
     }
@@ -72,11 +70,17 @@ apiClient.interceptors.response.use(
   (response) => response,
   async (error) => {
     const originalRequest = error.config;
+
     if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
       try {
         const refreshToken = Cookies.get(Namespace.BASE);
+        if (!refreshToken) {
+          removeTokensAndReload();
+          throw new Error("No refresh token found");
+        }
+
         const response = await axios.post(
           `${apiUri}/api/v1/auth/refresh`,
           { refreshToken },
@@ -84,12 +88,17 @@ apiClient.interceptors.response.use(
         );
 
         const { accessToken } = response.data;
-        localStorage.setItem(Namespace.BASE, accessToken);
+        if (!accessToken) {
+          removeTokensAndReload();
+          throw new Error("No token generated");
+        }
 
+        localStorage.setItem(Namespace.BASE, accessToken);
         originalRequest.headers.Authorization = `Bearer ${accessToken}`;
         return apiClient(originalRequest);
       } catch (refreshError) {
         console.error("Refresh token failed:", refreshError);
+        removeTokensAndReload();
         return Promise.reject(refreshError);
       }
     }
