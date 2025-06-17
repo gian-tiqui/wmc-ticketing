43ef4ca807b27ebfce1bbb4273621736c5c45493
gian-tiqui <gian.tiqui.dev@gmail.com>
2025-06-17 16:44:21 +0800

    categories table added

--- src/components/CategoriesTable.tsx
+++ src/components/CategoriesTable.tsx
@@ -1,5 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import useUserDataStore from "../@utils/store/userDataStore";
+import { getDepartmentCategoriesByDeptId } from "../@utils/services/departmentService";
+import { Category } from "../types/types";
+
 const CategoriesTable = () => {
-  return <div>table</div>;
+  const { user } = useUserDataStore();
+  const { data } = useQuery({
+    queryKey: [`department-categories-${user?.deptId}`],
+    queryFn: () =>
+      getDepartmentCategoriesByDeptId(user?.deptId, { limit: 100 }),
+    enabled: !!user && user != undefined,
+  });
+
+  return (
+    <div className="flex flex-col gap-2 px-4">
+      {data?.data.categories &&
+        data?.data.categories.map((category: Category) => (
+          <div key={category.id}>{JSON.stringify(category)}</div>
+        ))}
+    </div>
+  );
 };
 
 export default CategoriesTable;
